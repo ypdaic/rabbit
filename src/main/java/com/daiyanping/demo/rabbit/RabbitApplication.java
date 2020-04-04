167ec3567d4ccf1e401f2ad00270259442767378
daiyanping <daiyanping@sungotech.com>
2020-04-04 13:08:58 +0800

    代码提交。

--- src/main/java/com/daiyanping/demo/rabbit/RabbitApplication.java
+++ src/main/java/com/daiyanping/demo/rabbit/RabbitApplication.java
@@ -1,11 +1,14 @@
 package com.daiyanping.demo.rabbit;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 //@EnableRabbit
-@SpringBootApplication
+@MapperScan("com.daiyanping.demo.rabbit.mapper.**")
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class RabbitApplication {
 
     public static void main(String[] args) {
